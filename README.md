7bc6b949285af6d9398ce82dbedd304c4faab7cd
Sergio Obalski Filho <serginhobalski@gmail.com>
2022-10-05 20:49:24 -0300

    Agenda inicial

---
+++ README.md
@@ -0,0 +1,25 @@
+# Projeto Agenda #
+
+> Linguagem utilizada: **Python**
+> Framework: **Django***
+
+### Iniciando o projeto ###
+
+1) Após clonar o repositório, criar o ambiente virtual:
+
+    python -m venv venv
+
+
+2) Iniciar o ambiente virtual:
+
+    venv/Scripts/activate
+
+
+3) Instalar as dependências necessárias:
+
+    pip install django
+
+
+4) Rodar o projeto:
+
+    python manage.py runserver
---
+++ contatos/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import Categoria, Contato
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone',
+                    'email', 'categoria', 'data_criacao')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_per_page = 10
+    search_fields = ('nome', 'sobrenome')
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
